85ea026b0c95cdc21a3daa1ed5a3a677f7157bd9
maximeCony <maxime.cony@gmail.com>
2015-09-26 13:57:27 -0700

    hello React

--- public/scripts/src/app.js
+++ public/scripts/src/app.js
@@ -5,6 +5,7 @@ let ALGOLIA_SEARCH_ONLY_KEY = window.APP_GLOBALS.ALGOLIA_SEARCH_ONLY_KEY;
 import algoliasearch from 'algoliasearch';
 let client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_ONLY_KEY);
 let index = client.initIndex('app_store');
+import React from 'react';
 
 index
   .search('book')
@@ -12,3 +13,8 @@ index
     console.log(content);
   })
   .catch(console.error);
+
+React.render(
+  <h1>Hello, React!</h1>,
+  document.getElementById('example')
+);
