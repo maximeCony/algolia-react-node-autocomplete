5a3073a3f89b54f679f0e0b22f9a048e7b1d3f2e
maximeCony <maxime.cony@gmail.com>
2015-09-26 12:46:54 -0700

    test search

--- public/scripts/src/app.js
+++ public/scripts/src/app.js
@@ -3,5 +3,12 @@
 let ALGOLIA_APP_ID = window.APP_GLOBALS.ALGOLIA_APP_ID;
 let ALGOLIA_SEARCH_ONLY_KEY = window.APP_GLOBALS.ALGOLIA_SEARCH_ONLY_KEY;
 import algoliasearch from 'algoliasearch';
+let client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_ONLY_KEY);
+let index = client.initIndex('app_store');
 
-console.log(algoliasearch);
+index
+  .search('book')
+  .then((content) => {
+    console.log(content);
+  })
+  .catch(console.error);
